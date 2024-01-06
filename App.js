d9d99a36881597c52ddcaa427cb67fe1a120110c
deepak-singh-jethi <deepujethi@yahoo.com>
2024-01-06 22:37:30 +0530

    created create react app

--- App.js
+++ App.js
@@ -1,1 +1,15 @@
+import React from "react";
+import ReactDom from "react-dom/client";
 
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement(
+    "div",
+    { id: "child" },
+    React.createElement("h1", {}, "h1 inside the cascaded div's")
+  )
+);
+
+const root = ReactDom.createRoot(document.getElementById("root"));
+root.render(parent);
